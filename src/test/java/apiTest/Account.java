f3bd2e1e03083ff3706f4dc5a7f616a1e87113d7
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-06-14 17:32:14 -0300

    Desenvolvimento do projeto Testes de API

--- src/test/java/apiTest/Account.java
+++ src/test/java/apiTest/Account.java
@@ -4,6 +4,9 @@ package apiTest;
 
 import com.google.gson.Gson;
 import entities.AccountEntity;
+import entities.PetEntity;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -14,6 +17,11 @@ import static org.hamcrest.Matchers.is;
 // Classes
 public class Account {
     // Atributos
+    String userId;
+    Response resposta; // guardar o retorno da API
+    String ct = "application/json"; // contentType da API
+    String uri = "https://petstore.swagger.io/v2/"; // endereço base
+    String jsonBody; // guardar o json que será enviado
 
     // Instanciar Classes Externas
     Gson gson = new Gson(); // Instancia o objeto de conversao classe para json
@@ -41,19 +49,19 @@ public class Account {
         //String username = "Snoop"; // usuario
         //String password = "123456"; // senha
 
-        String jsonBody = gson.toJson(account); // Converte a entidade usuario em formato json
+        jsonBody = gson.toJson(account); // Converte a entidade usuario em formato json
 
 
         // Act - Executa
 
         // Dado - Quando - Entao
         // Given - When - Then
-        given() // dado
-                .contentType("application/json")        // tipo do conteudo
+        resposta = (Response) given() // dado
+                .contentType(ct)        // tipo do conteudo
                 .log().all()                            // registre tudo na ida
                 .body(jsonBody)                         // corpo da mensagem que será enviada
         .when() // quando
-                .post("https://petstore.swagger.io/v2/user")
+                .post(uri + "user")
 
         // Assert - Valida
         .then() // entao
@@ -63,5 +71,40 @@ public class Account {
         ; // fim da linha do REST assured
 
     }// fim do metodo de criacao do usuario
+
+    @Test
+    public void testAddPet(){
+        // Configura - Arrange
+        PetEntity account = new PetEntity();
+        jsonBody = gson.toJson(account);
+
+        account.id = 321;
+        account.category = "cat";
+        account.name = "Lagertha";
+        account.photoUrls = "total";
+        account.status = "available";
+
+
+
+        // Executa - Act
+        resposta = (Response) given() // dado
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+        .when() // quando
+                .post(uri + "pet")
+        .then()
+                .log().all()
+                .statusCode(200) // valida a comunicação
+                .body("status", is("available"))
+                .extract()
+        ; // fim do codigo testAddPet
+
+        // Extração do userId
+        userId = resposta.jsonPath().getString("userId");
+        // Valida - Assert
+        Assert.assertTrue(userId.length() != 0);
+    }// fim do metodo de add pet
+
 
 }
---
+++ src/test/java/entities/PetEntity.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class PetEntity {
+    public int id;
+    public String category;
+
+    public String name;
+    public String photoUrls;
+    public String tags;
+
+    public String status;
+
+}
